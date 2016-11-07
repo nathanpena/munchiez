bc0b7a3868e374ef95c852ad4b98ad97a9be1844
Nathan Pena <nnpena@outlook.com>
2016-11-06 20:24:43 -0500

    generate shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "1101e782338cdac7d9a5e6546667a461"
+  config.secret = "0421d9a195caa7591872522703b695bc"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
