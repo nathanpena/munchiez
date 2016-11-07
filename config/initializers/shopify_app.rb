8cbf333e717e92c12a35f3043af9c2af76711932
Nathan Pena <nnpena@outlook.com>
2016-11-06 21:25:03 -0500

    Update gem info.

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = "1101e782338cdac7d9a5e6546667a461"
   config.secret = "0421d9a195caa7591872522703b695bc"
-  config.redirect_uri = "https://munchiez.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
